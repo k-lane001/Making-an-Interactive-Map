69f579414fc979f98ccaa641366c73081bc82282
Kaylie Lane <k.lane5612@gmail.com>
2022-04-29 14:05:24 -0400

    cannot get code to return map at all

---
+++ map.js
@@ -0,0 +1,92 @@
+//map object
+
+const myMap = {
+        coordinates: [],
+        businesses: [],
+        map: {},
+        markers: {},
+
+
+        // Create map with Leaflet
+        buildMap() {     
+                this.map = L.map('map', {
+                center: this.coordinates,
+                zoom: 11,
+                });
+
+                //Create Map Layer with Open Street Map
+                L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                maxZoom: 18,
+                id: 'mapbox/streets-v11',
+                tileSize: 512,
+                zoomOffset: -1,
+                accessToken: 'pk.eyJ1Ijoia2xhbmUwMDYiLCJhIjoiY2wyZ21tYzI1MDVhNjNrbWtjc2R4ZHB0MSJ9.va6k8if1jCFE59ChveW3Uw'
+                }).addTo(this.map);
+                //create geolocation marker
+                const marker = L.marker(this.coordinates)
+                marker
+                .addTo(this.map)
+                .bindPopup('<p1><b>You are here</b><br><p1>')
+                .openPopup()
+        },
+
+	// add business markers
+	addMarkers() {
+		for (var i = 0; i < this.businesses.length; i++) {
+		this.markers = L.marker([
+			this.businesses[i].lat,
+			this.businesses[i].long,
+		])
+			.bindPopup(`<p1>${this.businesses[i].name}</p1>`)
+			.addTo(this.map)
+		}
+	},
+}
+async function getFoursquare(business) {
+	const options = {
+		method: 'GET',
+		headers: {
+		Accept: 'application/json',
+		Authorization: 'fsq3OI3QjTnFAJc+jlGFljDjFZJ5nJjT1S7ZiOKXQaXj5fU='
+		}
+	}
+	let limit = 5
+	let lat = myMap.coordinates[0]
+	let lon = myMap.coordinates[1]
+	let response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
+	let data = await response.text()
+	let parsedData = JSON.parse(data)
+	let businesses = parsedData.results
+	return businesses
+}
+// process foursquare array
+function processBusinesses(data) {
+	let businesses = data.map((element) => {
+		let location = {
+			name: element.name,
+			lat: element.geocodes.main.latitude,
+			long: element.geocodes.main.longitude
+		};
+		return location
+	})
+	return businesses
+}
+
+
+// event handlers
+// window load
+window.onload = async () => {
+	const coords = await getCoords()
+	myMap.coordinates = coords
+	myMap.buildMap()
+}
+
+// business submit button
+document.getElementById('submit').addEventListener('click', async (event) => {
+	event.preventDefault()
+	let business = document.getElementById('business').value
+	let data = await getFoursquare(business)
+	myMap.businesses = processBusinesses(data)
+	myMap.addMarkers()
+})
